3af942c68a671fd8da6744c60731c7c11da4e283
DriesBos <info@driesbos.com>
2021-01-03 19:42:58 +0000

    update: navigation component

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,11 +1,125 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <nuxt-link to="/" tag="li">Home</nuxt-link>
-        <nuxt-link to="/projects" tag="li">Projects</nuxt-link>
-        <nuxt-link to="/studio" tag="li">Studio</nuxt-link>
+  <header class="header">
+    <div class="mobile header-Mobile">
+      <TheStudio />
+    </div>
+    <div class="header-Main">
+      <div class="header-Main_Menu">
+        <span @click="toggleNav">Menu</span><span>|</span><span>Welcome</span>
+      </div>
+      <div class="logo header-Main_Logo">
+        <span>Unspoken </span><span>Agreement</span>
+      </div>
+      <TheStudio class="desktop" />
+    </div>
+    <div class="header-Nav" :class="{ active: nav }">
+      <ul class="header-Nav_List">
+        <nuxt-link class="header-Nav_ListItem" to="/studio" tag="li"
+          >Studio</nuxt-link
+        >
+        <nuxt-link class="header-Nav_ListItem" to="/projects" tag="li"
+          >Projects</nuxt-link
+        >
+        <nuxt-link class="header-Nav_ListItem" to="/contact" tag="li"
+          >Contact</nuxt-link
+        >
+        <nuxt-link class="header-Nav_ListItem" to="/client-portal" tag="li"
+          >Client Portal</nuxt-link
+        >
       </ul>
-    </nav>
+      <div class="header-Nav_Social">
+        <a href="https://www.instagram.com/unspoken.agreement/">Instagram</a>
+      </div>
+    </div>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      nav: false
+    }
+  },
+  methods: {
+    toggleNav() {
+      this.nav = !this.nav
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.header
+  position: fixed
+  left: 0
+  top: 0
+  right: 0
+  z-index: $zindex-header
+  padding: var(--spacing-side-two)
+  overflow: visible
+  @media screen and ( max-width: $breakpoint-mobile)
+    background: $white
+    color: $black
+    padding: var(--spacing-side-one)
+  a, a:hover
+    text-decoration: none
+  &-Mobile
+    border-bottom: $border
+    padding: var(--spacing-side-one)
+    padding-top: 0
+  &-Main
+    display: flex
+    justify-content: space-between
+    align-items: center
+    border-bottom: $border
+    padding: var(--spacing-side-one)
+    @media screen and ( min-width: $breakpoint-mobile)
+      border-top: $border
+    > div
+      flex-shrink: 0
+    &_Menu
+      @media screen and ( min-width: $breakpoint-mobile)
+        flex-grow: 2
+      > span:first-of-type
+        text-transform: uppercase
+        margin-right: .5rem
+        cursor: pointer
+      > span:nth-child(2)
+        @media screen and ( max-width: $breakpoint-mobile)
+          display: none
+      > span:last-of-type
+        font-family: $morion
+        margin-left: .5rem
+        @media screen and ( max-width: $breakpoint-mobile)
+          display: none
+    &_Logo
+      cursor: pointer
+      @media screen and ( min-width: $breakpoint-mobile)
+        flex-grow: 5
+        font-size: 1.5rem
+        span
+          display: block
+          line-height: 1
+  &-Nav
+    display: none
+    flex-wrap: nowrap
+    justify-content: space-between
+    align-items: center
+    overflow-x: auto
+    padding: var(--spacing-side-one)
+    padding-bottom: 0
+    &.active
+       display: flex
+    &_List
+      display: flex
+      flex-wrap: nowrap
+    &_ListItem
+      margin-right: 2rem
+      font-family: $morion
+      white-space: nowrap
+      cursor: pointer
+    &_Social a
+      font-family: $morion
+      cursor: pointer
+</style>
---
+++ components/TheStudio.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="studio">
+    <div class="studio-Row">
+      <div class="studio-Color">
+        <div class="studio-Color_Shape"></div>
+      </div>
+      <div class="studio-Location">Studio Colorado</div>
+      <div class="mobile studio-Time">2:33:06</div>
+      <div class="mobile studio-Degrees">82<span>°</span></div>
+    </div>
+    <div class="desktop studio-Row">
+      <div class="studio-Time">2:33:06</div>
+      <div class="studio-Degrees">82<span>°</span></div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+$ny: #619BF8
+$fl: #FFAF5A
+$col: #CEE583
+$ams: #E9DDCF
+
+.studio
+  display: inline-flex
+  flex-direction: column
+  line-height: 1.5
+  @media screen and ( max-width: $breakpoint-mobile)
+    display: flex
+  &-Row
+    display: flex
+    justify-content: space-between
+    > div
+      vertical-align: text-bottom
+      @media screen and ( max-width: $breakpoint-mobile)
+        margin-right: .5rem
+      &:last-child
+        margin-right: 0
+  &-Color
+    width: 1.5rem
+    display: flex
+    align-items: center
+    &_Shape
+      width: 1rem
+      height: 1rem
+      background: $col
+      border-radius: 1000px
+  &-Location
+    text-transform: uppercase
+    @media screen and ( max-width: $breakpoint-mobile)
+      flex-grow: 1
+  &-Time
+    margin-left: 1.5rem
+  &-Degrees
+    @media screen and ( max-width: $breakpoint-mobile)
+      margin-left: 1rem
+</style>
